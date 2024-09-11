897594dbc3f9f8b73569b5c9713f074f09a01482
CharlesPlachno <charlesplachno@outlook.com>
2024-09-10 17:31:22 -0700

    Added a generation selection dropdown display

--- script.js
+++ script.js
@@ -3,11 +3,19 @@ const answerIn = document.querySelector('input#answerIn');
 const answerBtn = document.querySelector('button#answerBtn');
 const nextPokeBtn = document.querySelector('.nextPoke');
 const collectionArea = document.querySelector('.collectionArea')
+const genDropBtn = document.querySelector('.genDropArea button')
+const genChecks = document.querySelectorAll('.genCheck')
+// TODO:
+// make sure at least one gen checkbox is always checked
+// make gen checkboxes functional
+// when you check a gen checkbox, add that range of numbers to unguessed - any that are already in guessed
+// when you uncheck a checkbox, remove any in that range from unchecked
 
 const guessedPokes = []
 let unguessedPokes = []
 let currentPoke = 0
 let currentTypeColors = []
+let dropDownStatus = false
 
 const typeColors = {
 	normal: '#A8A77A',
@@ -29,7 +37,17 @@ const typeColors = {
 	steel: '#B7B7CE',
 	fairy: '#D685AD',
 };
-
+const generationRanges = {
+    1: [1, 151],
+    2: [152, 251],
+    3: [252, 386],
+    4: [387, 493],
+    5: [494, 649],
+    6: [650, 721],
+    7: [722, 809],
+    8: [810, 905],
+    9: [906, 1025]
+}
 // Functions
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
@@ -153,6 +171,23 @@ function addPokeToCollection(){
     collectionArea.appendChild(newPokeCard)
     newPokeCard.appendChild(newPokeImg)
 }
+function updateDropDisplay(){
+    //Set the img for the dropdown button based on dropDownStatus
+    let chevronElement = genDropBtn.querySelector('i')
+    if(dropDownStatus){
+        chevronElement.className = 'fa-solid fa-chevron-up'
+        genChecks.forEach((genCheck) => {
+            genCheck.style.display = 'block'
+        })
+    }else {
+        chevronElement.className = 'fa-solid fa-chevron-down'
+        genChecks.forEach((genCheck) => {
+            genCheck.style.display = 'none'
+        })
+    }
+}
+
+
 
 // Add eventListeners
 answerBtn.addEventListener('click', () => {
@@ -176,8 +211,15 @@ answerIn.addEventListener('keydown', (event) => {
 nextPokeBtn.addEventListener('click', () => {
     nextPoke()
 })
+genDropBtn.addEventListener('click', () => {
+    genDropBtn.classList.toggle('active')
+    dropDownStatus = !dropDownStatus
+    updateDropDisplay()
+})
+
 
 // Page logic
 addPokeRange([1, 151])
 getRandomPokemon()
+updateDropDisplay()
 logState()
